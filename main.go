b373ef5a2edaef15eb1b604403e7eb3d6954c104
Anas Juwaidi <anas.didi95@gmail.com>
2021-01-26 20:49:27 +0800

    Add handler for request hello

--- main.go
+++ main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -11,14 +12,50 @@ func main() {
 	contextPath := mux.NewRouter().StrictSlash(true)
 	router := contextPath.PathPrefix("/urlshort").Subrouter()
 
-	router.HandleFunc("/", ArticlesCategoryHandler)
+	router.HandleFunc("/hello/{name}", getHelloWorld).Methods(http.MethodGet)
 
-	http.ListenAndServe("0.0.0.0:5000", router)
+	log.Printf("[main] Server started at %s:%s", "0.0.0.0", "5000")
+	log.Fatal(http.ListenAndServe("0.0.0.0:5000", router))
 }
 
-// ArticlesCategoryHandler TEST
-func ArticlesCategoryHandler(w http.ResponseWriter, r *http.Request) {
+func getHelloWorld(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Category: %v\n", vars["category"])
+	name := vars["name"]
+
+	query := r.URL.Query()
+	lang := query.Get("lang")
+
+	if len(lang) <= 0 {
+		lang = "en"
+	}
+
+	value := ""
+	switch lang {
+	case "bm":
+		value = "Selamat sejahtera, "
+	default:
+		value = "Hello, "
+		break
+	}
+
+	nameMap := map[string]interface{}{
+		"value": value + name,
+	}
+
+	sendResponse(w, http.StatusOK, nameMap, true, "Response returned successfully.")
+}
+
+func sendResponse(w http.ResponseWriter, statusCode int, data map[string]interface{}, isSuccess bool, message string) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+
+	responseBody := map[string]interface{}{
+		"status": map[string]interface{}{
+			"isSuccess": isSuccess,
+			"message":   message,
+		},
+		"data": data,
+	}
+
+	json.NewEncoder(w).Encode(responseBody)
 }
